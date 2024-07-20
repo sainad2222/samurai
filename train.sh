9123ae5eb291953bda068518d1537480105492f7
Sainath Singineedi <sainathsingineedi2222@gmail.com>
2024-07-20 12:08:58 +0530

    Refactor training script

---
+++ train.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python3 ./training/train_que_ans_1.py
+python3 ./training/train_ddl_1.py
--- training/train.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-python3 ./train_que_ans_1.py
-python3 ./train_ddl_1.py
