0d9007e34331268cacdba936eaba93788f34b759
Sainath Singineedi <sainathsingineedi2222@gmail.com>
2024-07-20 23:31:39 +0530

    Decrease max tokens size

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ session = boto3.Session()
 boto3_bedrock = boto3.client(service_name="bedrock-runtime")
 
 vn = Samurai(client=boto3_bedrock)
-vn.max_tokens = 20000
+vn.max_tokens = 4096
 
 vn.connect_to_snowflake_v2()
 
