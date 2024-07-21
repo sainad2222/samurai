15284f93e6089928c94d3dc22a094430186c8b05
Sainath Singineedi <sainathsingineedi2222@gmail.com>
2024-07-21 20:49:04 +0530

    override sql prompt

--- model/samurai.py
+++ model/samurai.py
@@ -21,9 +21,9 @@ class Samurai(Bedrock_Converse, ChromaDB_VectorStore, CustomSF):
         ChromaDB_VectorStore.__init__(
             self,
             config={
-                "n_results_sql": 6,
-                "n_results_documentation": 4,
-                "n_results_ddl": 20,
+                "n_results_sql": 20,
+                "n_results_documentation": 10,
+                "n_results_ddl": 40,
             },
         )
         Bedrock_Converse.__init__(
@@ -317,3 +317,74 @@ class Samurai(Bedrock_Converse, ChromaDB_VectorStore, CustomSF):
             fig.update_layout(template="plotly_white")
 
         return fig
+
+    def get_sql_prompt(
+        self,
+        initial_prompt : str,
+        question: str,
+        question_sql_list: list,
+        ddl_list: list,
+        doc_list: list,
+        **kwargs,
+    ):
+        """
+        Example:
+        ```python
+        vn.get_sql_prompt(
+            question="What are the top 10 customers by sales?",
+            question_sql_list=[{"question": "What are the top 10 customers by sales?", "sql": "SELECT * FROM customers ORDER BY sales DESC LIMIT 10"}],
+            ddl_list=["CREATE TABLE customers (id INT, name TEXT, sales DECIMAL)"],
+            doc_list=["The customers table contains information about customers and their sales."],
+        )
+
+        ```
+
+        This method is used to generate a prompt for the LLM to generate SQL.
+
+        Args:
+            question (str): The question to generate SQL for.
+            question_sql_list (list): A list of questions and their corresponding SQL statements.
+            ddl_list (list): A list of DDL statements.
+            doc_list (list): A list of documentation.
+
+        Returns:
+            any: The prompt for the LLM to generate SQL.
+        """
+
+        if initial_prompt is None:
+            initial_prompt = f"You are a {self.dialect} expert. " + \
+            "Please help to generate a SQL query to answer the question. Your response should ONLY be based on the given context and follow the response guidelines and format instructions. "
+
+        if self.static_documentation != "":
+            doc_list.append(self.static_documentation)
+
+        initial_prompt = self.add_documentation_to_prompt(
+            initial_prompt, doc_list, max_tokens=self.max_tokens
+        )
+
+        initial_prompt += (
+            "===Response Guidelines \n"
+            "1. If the provided context is sufficient, please generate a valid snowflake SQL query without any explanations for the question. \n"
+            "2. If the provided context is almost sufficient but requires knowledge of a specific string in a particular column, please generate an intermediate SQL query to find the distinct strings in that column. Prepend the query with a comment saying intermediate_sql \n"
+            "3. If the provided context is insufficient, please explain why it can't be generated. \n"
+            "4. Please use the most relevant table(s). \n"
+            "5. If the question has been asked and answered before, please repeat the answer exactly as it was given before. \n"
+        )
+        message_log = [self.system_message(initial_prompt)]
+        ddl_prompt = self.add_ddl_to_prompt(
+            "", ddl_list, max_tokens=self.max_tokens
+        )
+        message_log.append(self.user_message(ddl_prompt))
+        message_log.append(self.assistant_message("Acknowledged schema, now please send your query"))
+
+        for example in question_sql_list:
+            if example is None:
+                print("example is None")
+            else:
+                if example is not None and "question" in example and "sql" in example:
+                    message_log.append(self.user_message(example["question"]))
+                    message_log.append(self.assistant_message(example["sql"]))
+
+        message_log.append(self.user_message(question))
+
+        return message_log
